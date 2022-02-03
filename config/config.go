913a6770f62b45610c0b6c23656ef0986b254540
Ubuntu <ubuntu@ip-172-31-46-188.ap-northeast-3.compute.internal>
2022-02-03 04:48:05 +0000

    on server

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 func FetchConnection() *sql.DB {
-	connection := "root:123456789@tcp(127.0.0.1:3306)/event-planner?charset=utf8mb4&parseTime=True&loc=Local"
+	connection := "root:toor@tcp(172.17.0.1:3306)/event-planner?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := sql.Open("mysql", connection)
 	if err != nil {
